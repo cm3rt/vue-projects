24b5799f1deab58377607a6b05286f8cd93858f0
Joseph Alai <josephalai@gmail.com>
2018-02-24 17:00:01 -0800

    Filter Computed Search

--- vue-computed/src/App.vue
+++ vue-computed/src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h2>Cleantime Calculator Circle</h2>
-    <label>The days: <input type="text" v-model="days"></label><br />
-    <label>The years: <input type="text" @input="update" :value="getYears"></label><br />
-    <output>The days is {{days}} and the years is {{getYears}}</output><br />
-    <output>{{ getYears%365 != 0 ? (Math.trunc(getYears)) + " years " + " and " + (days%365) + " days." : ' Exactly.'}}</output>
+    <h2>Experiment Filter</h2>
+    <p>The computed filter, in this instance, is called when we used the v-for function, small in ComputedFilteredArray</p>
+    <p>If the 'in' is a computed function, it looks to that computed function.</p>
+    <p>Inside the computed function, we return this.ACTUALARRAY.filter(small =><i>if</i>  small.type != 'Defined')</p>
+    <h3>All</h3>
+    <ul>
+      <li v-for="exp in experiments">{{exp.name + ': ' + exp.type}}</li>
+    </ul>
+    <h3>Non Physics</h3>
+    <ul>
+      <li v-for="exp in nonPhysics">{{exp.name + ': ' + exp.type}}</li>
+    </ul>
+    <h3>Non CS</h3>
+    <ul>
+      <li v-for="exp in nonCS">{{exp.name + ': ' + exp.type}}</li>
+    </ul>
+    <h3>Custom</h3>
+    <label>Input Custom Filter for Experiment Type: <input type="text" v-model="filterType"></label><br />
+    <ul>
+      <li v-for="exp in customFilter">{{exp.name + ': ' + exp.type}}</li>
+    </ul>
   </div>
 </template>
 <script>
@@ -13,30 +29,31 @@ export default {
   name: 'app',
   data () {
     return {
-        days: 0,
+        filterType: 'CS',
+        experiments: [
+        { type: 'CS', name: 'Exp1'},
+        { type: 'CS', name: 'Exp1'},
+        { type: 'PHY', name: 'Exp3'},
+        { type: 'FS', name: 'Full Stack Experiment'},
+        ],
     }
-  },
+  },  
   computed: {
-    getYears: {
-      get() {
-        return this.days / 365
-      },
-      set (newValue) { 
-        this.days = newValue * 365 
-      }
-    }
-  },
-  methods: {
-    update (e) {
-      this.getYears = e.target.value
+    nonPhysics () {
+      return this.experiments.filter(smart => smart.type !== 'PHY')
+    },
+    nonCS() {
+      return this.experiments.filter(dumb => dumb.type !== 'CS')
+    },
+    customFilter() {
+      return this.experiments.filter(custom => custom.type.search(this.filterType) > -1)
     }
   }
-  
-  
 }
 </script>
 
 <style lang="scss">
+$pc:blue;
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -62,5 +79,8 @@ li {
 
 a {
   color: #42b983;
+}
+i{
+  color: $pc;
 }
 </style>
